83232d4cf3409f6bb582ea52f7d1da4b7b37a5fc
Dennis Nderitu <dknderitu@gmail.com>
2017-02-18 23:26:16 +0300

    working with more printing functions

---
+++ ex9.py
@@ -0,0 +1,23 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thur Fri Sat Sun"
+
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+
+print "Here are the days: ", days
+
+print "Here are the months: ", months
+
+
+print """
+
+There's something giong on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+
+"""
+
+# You have to type them like """ and not " " ", meaning with no spaces between each one.
+# Most programming errors in the beginning (and even later) are simple spelling mistakes, typos, or getting simple things out of order.
