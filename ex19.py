4daad5e615f4ced67cf87d2a1fc0733b5d58c873
Dennis Nderitu <dknderitu@gmail.com>
2017-02-22 13:28:13 +0300

    working with functions

--- ex19.py
+++ ex19.py
@@ -5,28 +5,30 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
 	print "Get a blanket.\n"
 
 
-
+# Using the function , you can give the functions directly
 print "We can just give the function numbers directly:"
 
 cheese_and_crackers(20,30)
 
-
+# Using the function , You can also use variables from your script
 print "OR, we can use variables from our script:"
 amount_of_cheese = 10 
 amount_of_crackers = 50
 
-
+# calling the function cheese_and_crackers
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
-
+# using the function to call cheese_and_crackers whern doing some math inside it
 print "We can even do math inside too:"
 cheese_and_crackers(10 + 20, 5 + 6)
 
+
+#Using the function , you can combine vatiables and math to achieve the result
 print "And we can combine the two, variables and math:"
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
 
 
-print "We cen dom some simple math inside:"
+print "We can do some simple math inside:"
 cheese_and_crackers(1000 + 500 , 900 + 250)
 
 print "We can also use variables in our script directly:"
