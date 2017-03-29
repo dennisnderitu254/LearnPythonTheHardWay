d6a6a642261e717d56f9e808df34dc80a892dc34
Dennis Nderitu <dknderitu@gmail.com>
2017-03-29 06:58:52 +0300

    modified the classes and the objects

--- oop43.py
+++ oop43.py
@@ -1,42 +1,71 @@
-class Scene(object):
-	 def enter(self):
-	 	print "This scene is not yet configured. Subclass it and implement enter()."
-	 	exit(1)
+#!/bin/python2
+# -*- coding: utf-8 -*-
+
+# ex43: Basic Object-Oriented Analysis and Design
+
+# Class Hierarchy
+# * Map
+#   - next_scene
+#   - opening_scene
+# * Engine
+#   - play
+# * Scene
+#   - enter
+#   * Death
+#   * Central Corridor
+#   * Laser Weapon Armory
+#   * The Bridge
+#   * Escape Pod
+# * Human
+#   - attack
+#   - defend
+#   * Hero
+#   * Monster
+
+from sys import exit
+from random import randint
+import time
+import math
 
 class Engine(object):
 
-    def __init__(self, scene_map):
+    def __init__(self, scene_map, hero):
         self.scene_map = scene_map
+        self.hero = hero
 
     def play(self):
         current_scene = self.scene_map.opening_scene()
-        last_scene = self.scene_map.next_scene('finished')
 
-        while current_scene != last_scene:
-            next_scene_name = current_scene.enter()
+        while True:
+            print "\n--------"
+            next_scene_name = current_scene.enter(self.hero)
             current_scene = self.scene_map.next_scene(next_scene_name)
 
-        # be sure to print out the last scene
-        current_scene.enter()
 
+
+
+class Scene(object):
+
+    def enter(self):
+        print "This scene is not yet configured. Subclass it and implement enter()."
+        exit(1)
 
 class Death(Scene):
 
     quips = [
-     "You died. You kinda suck at this.",
-     "Your mom would be proud...if she were smarter.",
-     "Such a luser",
-     "I have a small puppy that's better at this."
-     ]
+        "You died.  You kinda suck at this.",
+         "Your mom would be proud...if she were smarter.",
+         "Such a luser.",
+         "I have a small puppy that's better at this."
+    ]
 
-    def enter(self):
+    def enter(self, hero):
         print Death.quips[randint(0, len(self.quips)-1)]
         exit(1)
-		
 
 class CentralCorridor(Scene):
 
-    def enter(self):
+    def enter(self, hero):
         print "The Gothons of Planet Percal #25 have invaded your ship and destroyed"
         print "your entire crew.  You are the last surviving member and your last"
         print "mission is to get the neutron destruct bomb from the Weapons Armory,"
@@ -46,7 +75,7 @@ class CentralCorridor(Scene):
         print "You're running down the central corridor to the Weapons Armory when"
         print "a Gothon jumps out, red scaly skin, dark grimy teeth, and evil clown costume"
         print "flowing around his hate filled body.  He's blocking the door to the"
-        print "Armory and about to pull a weapon to blast you.You hase a couple of choices: shoot!, dodge! and tell a joke."
+        print "Armory and about to pull a weapon to blast you."
 
         action = raw_input("> ")
 
@@ -80,13 +109,10 @@ class CentralCorridor(Scene):
         else:
             print "DOES NOT COMPUTE!"
             return 'central_corridor'
-        
-
-
 
 class LaserWeaponArmory(Scene):
 
-    def enter(self):
+    def enter(self, hero):
         print "You do a dive roll into the Weapon Armory, crouch and scan the room"
         print "for more Gothons that might be hiding.  It's dead quiet, too quiet."
         print "You stand up and run to the far side of the room and find the"
@@ -94,14 +120,18 @@ class LaserWeaponArmory(Scene):
         print "and you need the code to get the bomb out.  If you get the code"
         print "wrong 10 times then the lock closes forever and you can't"
         print "get the bomb.  The code is 3 digits."
-        code = "%d%d%d" % (randint(1,9),  randint(1,9),  randint(1,9))
-        guess = raw_input("[keypad]>   ")
+        code = "%d%d%d" % (randint(1,9), randint(1,9), randint(1,9))
+
+        print code
+
         guesses = 0
 
-        while guess != code and guesses < 10:
+        while guesses < 10:
+            guess = raw_input("[keypad]> ")
+            if guess == code:
+                break
             print "BZZZZEDDD!"
             guesses += 1
-            guess = raw_input("[keypad]> ")
 
         if guess == code:
             print "The container clicks open and the seal breaks, letting gas out."
@@ -119,7 +149,7 @@ class LaserWeaponArmory(Scene):
 
 class TheBridge(Scene):
 
-    def enter(self):
+    def enter(self, hero):
         print "You burst onto the Bridge with the netron destruct bomb"
         print "under your arm and surprise 5 Gothons who are trying to"
         print "take control of the ship.  Each of them has an even uglier"
@@ -155,7 +185,7 @@ class TheBridge(Scene):
 
 class EscapePod(Scene):
 
-    def enter(self):
+    def enter(self, hero):
         print "You rush through the ship desperately trying to make it to"
         print "the escape pod before the whole ship explodes.  It seems like"
         print "hardly any Gothons are on the ship, so your run is clear of"
@@ -165,6 +195,7 @@ class EscapePod(Scene):
         print "do you take?"
 
         good_pod = randint(1,5)
+        print good_pod
         guess = raw_input("[pod #]> ")
 
 
@@ -182,14 +213,90 @@ class EscapePod(Scene):
             print "bright star, taking out the Gothon ship at the same"
             print "time.  You won!"
 
-            return 'finished'
+            return 'final_fight'
 
-class Finished(Scene):
+class Win(Scene):
+    ''' Win '''
 
-    def enter(self):
-        print "You won! Good job."
-        return 'finished'
+    def enter(self, hero):
 
+        print '''
+        You Win! Good Job!
+        '''
+
+        exit(0)
+
+class Final(Scene):
+
+    ''' final fight '''
+
+    def enter(self, hero):
+
+        # initialize a monster
+        monster = Monster("Gothon")
+
+        print "%s, You now came across the final boss %s! Let's fight!!!" % (hero.name, monster.name)
+
+
+        a_combat = Combat()
+
+        next_stage = a_combat.combat(hero, monster)
+        return next_stage
+
+class Combat(object):
+
+    def combat(self, hero, monster):
+
+        ''' combat between two roles '''
+
+        round = 1
+        while True:
+            print '='*30
+            print 'round %d' % round
+            print '='*30
+            print "Your HP: %d" % hero.hp
+            print "%s's HP: %d" % (monster.name, monster.hp)
+            print 'Which action do you want to take?'
+            print '-'*10
+            print '1) attack - Attack the enemy'
+            print '2) defend - Defend from being attacked, also will recover a bit'
+
+            try:
+                action = int(raw_input('> '))
+            except ValueError:
+                print "Please enter a number!!"
+                continue
+
+            # defending should be done before attacking
+            if action == 2:
+                hero.defend()
+
+            # action of monster, 1/5 possibility it will defends
+            monster_action = randint(1, 6)
+            if monster_action == 5:
+                monster.defend()
+
+            if action == 1:
+                hero.attack(monster)
+            elif action == 2:
+                pass
+            else:
+                print "No such action!"
+
+            if monster_action < 5:
+                monster.attack(hero)
+
+            # whether win or die
+            if hero.hp <= 0:
+                return 'death'
+
+            if monster.hp <= 0:
+                return 'win'
+
+            hero.rest()
+            monster.rest()
+
+            round += 1
 
 class Map(object):
 
@@ -199,21 +306,69 @@ class Map(object):
         'the_bridge': TheBridge(),
         'escape_pod': EscapePod(),
         'death': Death(),
-        'finished': Finished(),
+        'final_fight': Final(),
+        'win': Win()
     }
 
     def __init__(self, start_scene):
         self.start_scene = start_scene
 
     def next_scene(self, scene_name):
-        val = Map.scenes.get(scene_name)
-        return val
+        return Map.scenes.get(scene_name)
 
     def opening_scene(self):
         return self.next_scene(self.start_scene)
 
+class Human(object):
+
+    ''' class for human '''
+    defending = 0
+
+    def __init__(self, name):
+        self.name = name
+
+    def attack(self, target):
+        ''' attack the target '''
+        percent = 0
+        time.sleep(1)
+        if target.defending == 1:
+            percent = float(self.power) / 10.0 + randint(0, 10)
+            target.hp = math.floor(target.hp - percent)
+        else:
+            percent = float(self.power) / 5.0 + randint(0, 10)
+            target.hp = math.floor(target.hp - percent)
+        print "%s attack %s. %s's HP decreased by %d points." % (self.name, target.name, target.name, percent)
+
+    def defend(self):
+        ''' be in the defending state. '''
+        self.defending = 1
+        print "%s is trying to defend." % self.name
+
+    def rest(self):
+        ''' recover a bit after each round '''
+        if self.defending == 1:
+            percent = self.rate * 10 + randint(0, 10)
+        else:
+            percent = self.rate * 2 + randint(0, 10)
+        self.hp += percent
+        print "%s's HP increased by %d after rest." % (self.name, percent)
+        self.defending = 0
+
+
+class Hero(Human):
+    ''' class for hero '''
+
+    hp = 1000
+    power = 200
+    rate = 5
+
+class Monster(Human):
+    ''' class for monster '''
+    hp = 5000
+    power = 250
+    rate = 5
+
 a_map = Map('central_corridor')
-
-a_game = Engine(a_map)
-
+a_hero = Hero('Joe')
+a_game = Engine(a_map, a_hero)
 a_game.play()
